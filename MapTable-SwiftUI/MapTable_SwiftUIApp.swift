94ac9da97f1e00cde25f3b04c5290932e222e09e
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-06-29 15:36:08 -0300

    Initial Commit

---
+++ MapTable-SwiftUI/MapTable_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTable_SwiftUIApp.swift
+//  MapTable-SwiftUI
+//
+//  Created by sebastiao Gazolla Costa Junior on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MapTable_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
